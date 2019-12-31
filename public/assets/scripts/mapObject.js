db26b3f9c2118d4dea1473e3bf72ff404b598b76
Michael Sagnes <michael@northernweb.no>
2019-12-31 21:19:38 +0100

    Render snake FoV
    Created function to draw the snake field of view blocks.

--- public/assets/scripts/mapObject.js
+++ public/assets/scripts/mapObject.js
@@ -69,4 +69,11 @@ export class Map {
     this.ctx.fillStyle = food.color;
     this.ctx.fillRect(food.x, food.y, this.GetBlockSize(), this.GetBlockSize());
   }
+
+  DrawSnakeVision (snake) {
+    for (let i = 0; i < snake.fovPositions; i++) {
+      this.ctx.fillStyle = '#90ee90';
+      this.ctx.fillRect(snake.fovPositions[i].x, snake.fovPositions[i].y, this.GetBlockSize(), this.GetBlockSize());
+    }
+  }
 }
